f048456cac77f69f263274a1fc6cf322b0a8a748
PavelSapegin <pawsapegin@yandex.ru>
2026-09-05 12:24:55 +0300

    Update model path in config

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 class Settings(BaseSettings):
 
-    MODEL_DIR: Path = BASE_DIR / "data/models"
+    MODEL_DIR: Path = BASE_DIR / "models"
 
     @property
     def MODEL_PATH(self) -> Path:
