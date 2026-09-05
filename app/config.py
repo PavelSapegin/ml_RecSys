7cdaeac890710402b64dbc10080eac7d69740878
PavelSapegin <pawsapegin@yandex.ru>
2026-09-05 11:56:00 +0300

    Add config file for app

---
+++ app/config.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+class Settings(BaseSettings):
+
+    MODEL_DIR: Path = BASE_DIR / "data/models"
+
+    @property
+    def MODEL_PATH(self) -> Path:
+        return self.MODEL_DIR / "hybrid_model.joblib"
+
+    class Config:
+        env_file = ".env"
+
+
+config = Settings()    
